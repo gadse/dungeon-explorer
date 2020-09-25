a835d3b6c0324623dfb6f6ed57157a15b1f43edd
Peter Bozsoky <peter-bozsoky@posteo.de>
2020-09-25 14:48:34 +0200

    hello: world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dungeon_explorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
